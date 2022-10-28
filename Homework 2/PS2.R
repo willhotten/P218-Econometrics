986a6adf20aea311c5377a0cfd5b3341d815fe5d
willhotten <76008118+willhotten@users.noreply.github.com>
2022-10-28 12:49:04 +0100

    Code and results for q3

--- Homework 2/PS2.R
+++ Homework 2/PS2.R
@@ -11,7 +11,7 @@
 rm(list=ls())
 
 # Define a vector of packages that the script will use
-pkgs_required <- c("AER", "estimatr", "tidyverse", "vtable", "fastDummies", "stargazer")
+pkgs_required <- c("AER", "estimatr", "tidyverse", "vtable", "fastDummies", "stargazer", "ggplot2")
 pkgs_install  <- pkgs_required[!(pkgs_required %in% installed.packages()[,"Package"])]
 
 # If any package is not already install then install it in machine
@@ -28,40 +28,40 @@ invisible(lapply(pkgs_required, library, character.only=TRUE))
 ##################################### Q2 #######################################
 ################################################################################
 
-# Importing dataset
+# Import dataset
 df <- read.delim("Homework 2/ps1.dat", sep="")
 
-# Generating new variables for log wage, education and experience
+# Generate new variables for log wage, education and experience
 df$logw0 <- log(df$w0)
 df$educ <- df$ed0
 df$exper <- df$a0 - (df$educ + 6)
 
-# Creating dummy variables for each of the education variables and each of the experience variables
+# Create dummy variables for each of the education variables and each of the experience variables
 df_dummy <- dummy_cols(df, select_columns = c("educ", "exper"))
 
-# Dropping columns not relevant for regression
+# Drop columns not relevant for regression
 df_dummy2 <- subset(df_dummy, select = -c(w0, ed0, a0, educ, exper))
 
-# Running regression on dummy variable set
+# Run regression on dummy variable set
 reg_dummy2 <- lm(logw0 ~ ., data=df_dummy2)
 
-# To produce table of results for LaTeX
+# Produce table of results for LaTeX
 stargazer(reg_dummy2, title="Results", align=TRUE)
 
 ################################################################################
 ##################################### Q3 #######################################
 ################################################################################
 
-# Specifying true parameter values based on true model: y_i = 1 + 0.5z_i + e_i
+# Specify true parameter values based on true model: y_i = 1 + 0.5z_i + e_i
 alpha = 1
 beta = 0.5 
 
-# Setting seed, sample size (n) and no. iterations (M)
+# Set seed, sample size (n) and no. iterations (M)
 set.seed(1)
 n = 100
 M = 1000
 
-# Setting up stores of parameter estimates
+# Setup stores of parameter estimates
 intercept <- rep(0,M)
 slope <- rep(0,M)
 sigmasq_hat <- rep(0,M)
@@ -91,4 +91,33 @@ for (i in 1:M){
 estimates <- data.frame(intercept, slope, sigmasq_hat)
 
 # Produce table of summary statistics to input into LaTeX
-stargazer(estimates, title = "Monte Carlo simulation results, N = 100, M = 1,000", align =TRUE)
+stargazer(estimates, title = paste("Monte Carlo simulation results, n = ", n, ", M = ", M, sep=""), align =TRUE, out=paste("Homework 2/MC_results_n", n, "_M", M,".tex", sep = ""))
+
+# Produce and save histograms of regression coefficients
+
+# Intercept histogram
+hist1 <- ggplot(estimates, aes(x=intercept)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white")+
+  geom_density(alpha=.2, fill="#FF6666")
+ggsave(paste("Homework 2/hist_intercept_n",n,"_M",M,".png", sep=""))
+
+# Slope histogram
+hist2 <- ggplot(estimates, aes(x=slope)) + 
+  geom_histogram(aes(y=..density..), colour="black", fill="white")+
+  geom_density(alpha=.2, fill="#FF6666")
+ggsave(paste("Homework 2/hist_slope_n",n,"_M",M,".png", sep=""))
+
+# Compare empirial cdf of slope estimate to normal cdf
+
+# Asymptotic variance for normal cdf
+avar = (1/3)/(6*n)
+
+# Plotting empirical cdf and normal cdf with mean = 1 and sd = asymptotic variance defined above 
+ecdf <- ggplot(estimates, aes(x=slope)) +
+  stat_ecdf(geom = "step") +
+  stat_function(fun=pnorm,color="blue", args = list(mean = beta, sd = sqrt(avar))) +
+  labs(title="ECDF and theoretical CDF")
+ggsave(paste("Homework 2/ecdf_slope_n",n,"_M",M,".png", sep=""))
+
+
+
